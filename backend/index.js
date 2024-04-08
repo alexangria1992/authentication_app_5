483946c378f36c0aecd3da5e95becea9ac976b81
Alexandre Angrignon <alex040892@gmail.com>
2024-04-08 14:28:53 +1200

    Successful Registration & tested in postman

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,24 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+//Registration Endpoint
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+
+  //Match the password
+  const hashedPassword = await bcrypt.hash(password, 10);
+  console.log(hashedPassword);
+
+  const sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+  db.query(sql, [username, hashedPassword], (err, result) => {
+    if (err) {
+      console.log("Error in Registration:" + err);
+    } else {
+      res.json({ message: "Registration Successful" });
+    }
+  });
+});
+
 // PORT
 const port = 5500;
 app.listen(port, () => {
